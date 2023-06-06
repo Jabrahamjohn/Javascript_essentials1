be0729f7fd43021a436b6a22910a7feccb4726ec
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-06-07 02:05:58 +0300

    variables in js

---
+++ initializingvars.js
@@ -0,0 +1,9 @@
+let height = 180;
+let anotherHeight = height + 20;
+let weight;
+console.log(height);
+console.log(anotherHeight);
+weight = 80;
+console.log(weight);
+//-> the below line will be printed as a text
+console.log("height");
---
+++ variables.js
@@ -0,0 +1,8 @@
+var height;
+console.log(height);
+console.log(weight);
+//->thiswill print error: undefined and reference error: weight is not defined
+let height;
+console.log(height);
+//->this above lines will print error: undefined
+//->let prevents the variable from being used before it is declared
