7bbdd91bd0a94c700c15559f86cf930d740880c6
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-06-07 02:35:30 +0300

    constants in js

---
+++ constants.js
@@ -0,0 +1,10 @@
+"use strict";
+const WELCOME = "WELCOME TO AJNETS";
+console.log(WELCOME);
+//->The const keyword is used to declare containers similar to variables. 
+//->Such containers are called constants. Constants are also used to store certain values, but once values have been entered into them during initialization, they can no longer be modified.
+//->This means that this type of container is simultaneously declared and initialized
+
+//->a change in the constant is impossible.
+const greeting = "WELCOME TO AJNETS";
+console.log(greeting);
